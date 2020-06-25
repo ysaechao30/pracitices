ac558815d2df2333a8570401ee20126138c12526
ysaechao30 <59863962+ysaechao30@users.noreply.github.com>
2020-06-24 20:07:27 -0700

    Add files via upload

---
+++ practices/JS/javaS.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    var x = document.cookie;
+document.getElementById("demo").innerHTML = "Cookies associated with this document:" + x; 
+}
---
+++ practices/JS/js_1.js
@@ -0,0 +1,4 @@
+function Hello_World_Function() {
+    var A = document.getElementsByClassName("Click");
+    A[0]. innerHTML="The text has changed!";
+}
